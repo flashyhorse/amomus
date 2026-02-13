a182b96a9855c598f2db40ac9885ed9286941049
flashyhorse <danilo_doebber@estudante.sesisenai.org.br>
2026-02-12 21:34:36 -0300

    trabalho

---
+++ teste.py
@@ -0,0 +1,9 @@
+nome = (str("danilo"))
+
+idade = (int(21))
+
+convenio = (bool(True))
+
+valor_consulta = (float(150.50))
+
+print(f"seus dados de usúario {nome}, \n, {idade}, \n, {convenio}, \n, {valor_consulta}")
---
+++ trabalho.py
@@ -0,0 +1,11 @@
+sistema = "estoque"
+
+quantidade = "pizza"
+
+disponivel = False
+
+quantidade = 50
+
+valor = 44.50
+
+print(f"══════════════════════════ \n FICHA DO ESTOQUE \n══════════════════════════ \n ITEM: {quantidade} \n QUANTIDADE: {quantidade}\n DISPONIBILIDADE: {disponivel} \n VALOR: {valor}R$")
