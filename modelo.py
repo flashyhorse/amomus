8d96af5e50d080243cb6f0a7dc44cb1f5cd7f711
flashyhorse <danilo_doebber@estudante.sesisenai.org.br>
2026-02-19 21:18:43 -0300

    modelo.py

---
+++ modelo.py
@@ -0,0 +1,24 @@
+""""
+GESTÃO DE ESTOQUE
+
+Cliente
+
+Nome: Identificação de usuário.
+
+CPF/CNPJ: Registro de identificação fiscal do cliente.
+
+Contato: Email/Telefone para manter comunicação com o cliente.
+
+Fidelidade: Tempo no qual o cliente se mantém comprando com a empresa
+
+
+CONTRATO
+
+
+
+Produto: Pedido do material solicitado
+
+Quantidade: Quantia no qual é exigida
+
+Valor: 
+""""
